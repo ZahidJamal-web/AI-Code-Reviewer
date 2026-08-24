642f2bc93966b6f86a4a7394e5c6099946c24003
ZahidJamal-web <zahdjamal4027@gmail.com>
2026-08-24 19:53:46 +0530

    Deleted unused Code

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'es2022' 
-  },
-  esbuild: {
-    supported: {
-      'top-level-await': true 
-    }
-  },
+
   server: {
     port: 5173,
     host: "localhost"
   }
 });
-
